bcd0d05e5467682d64f1bbae7cce0fb93a36403f
wolim99 <80511449+wolim99@users.noreply.github.com>
2024-01-18 00:28:31 +0900

    2024.01.17과제

--- hgjava/src/com/yedam/todo/Todo117.java
+++ hgjava/src/com/yedam/todo/Todo117.java
@@ -5,19 +5,29 @@ import java.util.Scanner;
 
 public class Todo117 {
 	public static void main(String[] args) {
-		// 친구3명의 이름과 연락처를 입력하도록 메세지출력.
-		// 입력완료.
-		
-		// 이름 : 홍길동
-		// 연락처: 010-1111-2222
-		// ===================
-		// 이름 : 김길동
-		// 연락처: 010-1111-2222
-		// ===================
-		// 이름 : 박길동
-		// 연락처: 010-1111-2222
-		// ===================
-		
-	}
+        Scanner scn = new Scanner(System.in);
+
+        String[] names = new String[3];
+        String[] contacts = new String[3];
+
+        // 친구 3명의 이름과 연락처 입력
+        for (int i = 0; i < 3; i++) {
+            System.out.println((i + 1) + "번째 친구의 이름을 입력하세요:");
+            names[i] = scn.nextLine();
+            System.out.println((i + 1) + "번째 친구의 연락처를 입력하세요:");
+            contacts[i] = scn.nextLine();
+        }
+
+        System.out.println("입력완료.\n");
+
+        // 입력받은 정보 출력
+        for (int i = 0; i < 3; i++) {
+            System.out.println("이름 : " + names[i]);
+            System.out.println("연락처: " + contacts[i]);
+            System.out.println("===================");
+        }
+
+        scn.close();
+    }
 
 }
