2e24c4f97edd089150d522af4eb1944db6998dba
admin <admin@DESKTOP-ERD09N0>
2024-01-17 09:05:22 +0900

    수정

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -4,15 +4,27 @@ import java.util.Scanner;
 
 
 public class Todo0116 {
-	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		
-		System.out.println("값을 입력>>>");
-		String input = scn.nextLine(); 
-		// input.equals("quit") 
-		
-		// 입력값은 홍길동 입니다.
-		
-		// "quit" 값이 들어오면 << end of prog>>
+	public class Main {
+	    public static void main(String[] args) {
+	        Scanner scn = new Scanner(System.in);
+
+	        while (true) {
+	            System.out.println("값을 입력>>>");
+	            String input = scn.nextLine();
+
+	            // 입력값이 "quit"일 경우 프로그램 종료
+	            if (input.equals("quit")) {
+	                
+	                break;
+	            }
+	            System.out.printf("입력값은 %s 입니다", input);
+	            System.out.println();
+
+	            // 입력값 처리 (예: 홍길동 처리)
+	            // 여기서 입력값에 대한 추가적인 처리를 할 수 있습니다.
+	        }
+	        System.out.println("<< end of prog >>");
+	        scn.close();
+	    }
 	}
 }
