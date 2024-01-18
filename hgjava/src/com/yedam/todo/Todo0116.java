ce64c6d5ba943a59226cabdcebc7fd007ef904bd
admin <admin@DESKTOP-ERD09N0>
2024-01-18 09:37:13 +0900

    과제 수정

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -1,36 +1,28 @@
-//package com.yedam.todo;
-//
-//
-//import java.util.Scanner;
-//
-//public class Todo0116 {
-//
-//	public static void main(String[] args) {
-//		Scanner scn = new Scanner(System.in);
-//
-//		while (true) {
-//			System.out.println("값을 입력>>>");
-//			String input = scn.nextLine();
-//
-//			// 입력값이 "quit"일 경우 프로그램 종료
-//			if (input.equals("quit")) {
-//
-//				break;
-//			}
-//			System.out.printf("입력값은 %s 입니다", input);
-//			System.out.println();
-//
-//			// 입력값 처리 (예: 홍길동 처리)
-//			// 여기서 입력값에 대한 추가적인 처리를 할 수 있습니다.
-//		}
-//		System.out.println("<< end of prog >>");
-//		scn.close();
-//
-//
-//>>>>>>> branch 'master' of https://github.com/wolim99/hgjava.git
-//	}
-//<<<<<<< HEAD
-//
-//=======
-//>>>>>>> branch 'master' of https://github.com/wolim99/hgjava.git
-//}
+package com.yedam.todo;
+
+import java.util.Scanner;
+
+public class Todo0116 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("값을 입력>>>");
+			String input = scn.nextLine();
+
+			// 입력값이 "quit"일 경우 프로그램 종료
+			if (input.equals("quit")) {
+				break;
+			}
+			System.out.printf("입력값은 %s 입니다", input);
+			System.out.println();
+
+			// 입력값 처리 (예: 홍길동 처리)
+			// 여기서 입력값에 대한 추가적인 처리를 할 수 있습니다.
+		}
+		System.out.println("<< end of prog >>");
+		scn.close();
+
+	}
+
+}
--- hgjava/src/com/yedam/todo/Todo117.java
+++ hgjava/src/com/yedam/todo/Todo117.java
@@ -1,33 +1,58 @@
 package com.yedam.todo;
 
-
 import java.util.Scanner;
 
 public class Todo117 {
 	public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
+		// 강사님 답안
+		Scanner scn = new Scanner(System.in);
 
-        String[] names = new String[3];
-        String[] contacts = new String[3];
+		String[] strAry = new String[6];
+		int idx = 0;
+		for (int i = 1; i < 3; i++) {
+			System.out.println("이름>>>");
+			strAry[idx++] = scn.nextLine();
+			System.out.println("연락처>>>");
+			strAry[idx++] = scn.nextLine(); // 0번방에서 바로 입력
+		}
+		for (int i = 0; i < strAry.length; i++) {
+			if (i % 2 == 0) { // 이름출력
+				System.out.println("이름: " + strAry[i]);
+			} else {
+				System.out.println("연락처: " + strAry[i]);
+				System.out.println("===================");
+			}
+		}
 
-        // 친구 3명의 이름과 연락처 입력
-        for (int i = 0; i < 3; i++) {
-            System.out.println((i + 1) + "번째 친구의 이름을 입력하세요:");
-            names[i] = scn.nextLine();
-            System.out.println((i + 1) + "번째 친구의 연락처를 입력하세요:");
-            contacts[i] = scn.nextLine();
-        }
 
-        System.out.println("입력완료.\n");
+	}
 
-        // 입력받은 정보 출력
-        for (int i = 0; i < 3; i++) {
-            System.out.println("이름 : " + names[i]);
-            System.out.println("연락처: " + contacts[i]);
-            System.out.println("===================");
-        }
+	public static void method01() {
+		// 내 답안
+		{
+			Scanner scn = new Scanner(System.in);
 
-        scn.close();
-    }
+			String[] names = new String[6];
+			String[] contacts = new String[6];
 
+			// 친구 3명의 이름과 연락처 입력
+			for (int i = 0; i < 3; i++) {
+				System.out.println((i + 1) + "번째 친구의 이름을 입력하세요:");
+				names[i] = scn.nextLine();
+				System.out.println((i + 1) + "번째 친구의 연락처를 입력하세요:");
+				contacts[i] = scn.nextLine();
+			}
+
+			System.out.println("입력완료.\n");
+
+			// 입력받은 정보 출력
+			for (int i = 0; i < 3; i++) {
+				System.out.println("이름 : " + names[i]);
+				System.out.println("연락처: " + contacts[i]);
+				System.out.println("===================");
+			}
+
+			scn.close();
+		}
+	}
 }
